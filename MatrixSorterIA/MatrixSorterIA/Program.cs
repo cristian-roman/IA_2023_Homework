cb1c40b859a7185810f1ffbd553da21c19b59782
Cristian Roman <romancristianroman@yahoo.com>
2023-10-17 14:34:44 +0300

    .NET start

---
+++ MatrixSorterIA/MatrixSorterIA/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MatrixSorterIA
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("start");
+        }
+    }
+}
